b23608037c8e2be675eb5e25042d610797fa4178
Ubuntu <ubuntu@ip-172-31-6-117.ap-southeast-2.compute.internal>
2015-02-13 02:25:12 +0000

    init

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Control.Applicative
+import qualified Data.HashMap.Strict as H
+import qualified Data.ByteString.Char8 as C
+import           Control.Applicative ((<|>))
 import           Snap.Core
 import           Snap.Util.FileServe
-import           Snap.Http.Server
+import           Snap.Http.Server (quickHttpServe)
+import           Snap.Util.GZip (withCompression)
 
 main :: IO ()
-main = quickHttpServe site
+main = quickHttpServe $ withCompression site
 
 site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
+site = routes <|> handles <|> staticContent <|> notFound
+    where
+      routes        = page "blog" <|> page "purescript-jquery-setup-example" <|>
+                      page "drag-and-drop-with-baconjs" <|> page "googlyfish" <|>
+                      page "googlyapps"
+      page x        = route [(C.pack x, serveFile $ "static/" ++ x ++ ".html")]
+      handles       = route[("chrees", writeBS "thanks :)")]
+      staticContent = serveDirectory' "static"
+      notFound      = serveFile "static/404.html"
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+serveDirectory' :: MonadSnap m => FilePath -> m ()
+serveDirectory' = serveDirectoryWith config
+    where
+        config = fancyDirectoryConfig {
+              indexFiles = ["index.html"]
+            , mimeTypes  = newMimeTypes `H.union` defaultMimeTypes
+            }
+
+        newMimeTypes = H.fromList [
+                 (".appcache", "text/cache-manifest")
+               , (".ttf",      "font/truetype")
+               , (".otf",      "font/opentype")
+               , (".eot",      "application/vnd.ms-fontobject")
+               , (".woff",     "application/font-woff")
+               ]
